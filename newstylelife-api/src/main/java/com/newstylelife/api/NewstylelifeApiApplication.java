5f6374a7e95f8c2fa935aa37d710ca4bd2eb2609
Wilson Amorim <wilson.amorim@qintess.com>
2023-09-04 18:13:45 -0300

    Primeiro commit

---
+++ newstylelife-api/src/main/java/com/newstylelife/api/NewstylelifeApiApplication.java
@@ -0,0 +1,13 @@
+package com.newstylelife.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewstylelifeApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewstylelifeApiApplication.class, args);
+	}
+
+}
---
+++ newstylelife-api/src/test/java/com/newstylelife/api/NewstylelifeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.newstylelife.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewstylelifeApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
