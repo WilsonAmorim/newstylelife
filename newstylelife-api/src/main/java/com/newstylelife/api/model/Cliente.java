820c63ec7a5ac9f68b048a466226f25466166bdc
Wilson Amorim <wilson.amorim@qintess.com>
2023-09-04 18:27:43 -0300

    Alteracao modelo e a classe cliente

--- newstylelife-api/src/main/java/com/newstylelife/api/model/Cliente.java
+++ newstylelife-api/src/main/java/com/newstylelife/api/model/Cliente.java
@@ -1,5 +1,58 @@
 package com.newstylelife.api.model;
 
 public class Cliente {
+	private String cpfCliente;
+	private String nome;
+	private String email;
+	private String celular;
+	private boolean status;
+	
+		
+	public Cliente(String cpfCliente, String nome, String email, String celular, boolean status) {
+		this.cpfCliente = cpfCliente;
+		this.nome = nome;
+		this.email = email;
+		this.celular = celular;
+		this.status = status;
+	}
+	
+	public String getCpfCliente() {
+		return cpfCliente;
+	}
+	public void setCpfCliente(String cpfCliente) {
+		this.cpfCliente = cpfCliente;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getCelular() {
+		return celular;
+	}
+	public void setCelular(String celular) {
+		this.celular = celular;
+	}
+	public boolean isStatus() {
+		return status;
+	}
+	public void setStatus(boolean status) {
+		this.status = status;
+	}
 
+	@Override
+	public String toString() {
+		return "Cliente [cpfCliente=" + cpfCliente + ", nome=" + nome + ", email=" + email + ", celular=" + celular
+				+ ", status=" + status + "]";
+	}
+	
+	
+	
 }
